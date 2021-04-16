3b779dc7072b9701a7db3687182e16b41a039145
beendra <alexandra.ricardel@gmail.com>
2021-04-15 21:08:17 -0500

    adding

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Post
+
+    attr_accessor :title, :author
+
+    @@all  = []
+
+    def  initialize(title)
+        @title = title
+        @author = author
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        author.name if author
+    end
+
+end
